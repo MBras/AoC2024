2bdd0b7ba911343f5e9a807431808264025c2282
Mark Braspenning <bras@sandcat.nl>
2024-12-04 09:34:46 +0100

    failing day 3 part 1, but why?

---
+++ 4/1.py
@@ -0,0 +1,36 @@
+import sys
+import re
+
+# Check if the filename is provided
+if len(sys.argv) < 2:
+    print("Usage: python 1.py <filename>")
+    sys.exit(1)
+
+# Get the filename from the command line arguments
+filename = sys.argv[1]
+
+# read lines
+with open(filename, "r") as file:
+    lines = file.readlines()
+
+dirs = [[0,-1],[0,1],[1,-1],[1,0],[1,1],[-1,-1],[-1,0],[-1,1]]
+
+def searchXMAS(startX, startY):
+    count = 0
+    for direction in dirs:
+        try:
+            if lines[startX + direction[0]][startY + direction[1]] == "M" and lines[startX + 2 * direction[0]][startY + 2 * direction[1]] == "A" and lines[startX + 3 * direction[0]][startY + 3 * direction[1]] == "S":
+                print(direction)
+                count += 1
+        except:
+            pass 
+    return count
+
+result = 0
+for x in range(len(lines)):
+    for y in range(len(lines[x])):
+        if lines[x][y] == "X":
+            print("(" + str(x) + "," + str(y) + ")") 
+            result += searchXMAS(x, y)
+
+print(result)
