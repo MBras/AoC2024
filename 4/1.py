c95cbb53ef55eef7c12aef0d0a2ffa9dcc3c53a8
Mark Braspenning <bras@sandcat.nl>
2024-12-04 11:06:45 +0100

    Boundary check hack in the list class

--- 4/1.py
+++ 4/1.py
@@ -9,28 +9,38 @@ if len(sys.argv) < 2:
 # Get the filename from the command line arguments
 filename = sys.argv[1]
 
+class SafeList(list):
+    def __getitem__(self, index):
+        if index < 0:
+            raise ValueError("Negative indices are not allowed.")
+        return super(SafeList, self).__getitem__(index)
+
 # read lines
 with open(filename, "r") as file:
-    lines = file.readlines()
+    lines = SafeList(file.readlines())
 
 dirs = [[0,-1],[0,1],[1,-1],[1,0],[1,1],[-1,-1],[-1,0],[-1,1]]
 
+
+
 def searchXMAS(startX, startY):
     count = 0
+    
     for direction in dirs:
         try:
             if lines[startX + direction[0]][startY + direction[1]] == "M" and lines[startX + 2 * direction[0]][startY + 2 * direction[1]] == "A" and lines[startX + 3 * direction[0]][startY + 3 * direction[1]] == "S":
-                print(direction)
+                #print(direction)
                 count += 1
         except:
             pass 
     return count
 
+# Part 1
 result = 0
 for x in range(len(lines)):
     for y in range(len(lines[x])):
         if lines[x][y] == "X":
-            print("(" + str(x) + "," + str(y) + ")") 
+            #print("(" + str(x) + "," + str(y) + ")") 
             result += searchXMAS(x, y)
 
 print(result)
