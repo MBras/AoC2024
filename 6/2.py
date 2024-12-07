3314ab1fb9b6522cb41d2b682c2da22554571caf
Mark Braspenning <bras@sandcat.nl>
2024-12-07 13:35:24 +0100

    hopefully the solution to day 6.2

--- 6/2.py
+++ 6/2.py
@@ -1,5 +1,6 @@
 import sys
-import fnmatch
+import re
+import copy
 
 # Check if the filename is provided
 if len(sys.argv) < 2:
@@ -9,29 +10,104 @@ if len(sys.argv) < 2:
 # Get the filename from the command line arguments
 filename = sys.argv[1]
 
-# read lines
+# read lines, remove linefeed
 with open(filename, "r") as file:
-    lines = file.readlines()
+    lines = [list(line.strip()) for line in file.readlines()]
 
-xmas = [["M?M","S?S"], ["M?S","M?S"], ["S?S","M?M"], ["S?M", "S?M"]]
+def printmap(map):
+    print ""
+    print('\n'.join(''.join(line) for line in map))
 
-def searchXMAS(startX, startY):
-    top = lines[startX - 1][startY - 1:startY + 2]
-    bottom = lines[startX + 1][startY - 1:startY + 2]
-    for a in xmas:
-        if fnmatch.fnmatch(top, a[0]) and fnmatch.fnmatch(bottom, a[1]):
-            print("Found it")
-            print top
-            print bottom
-            print a
-            return 1
-    return 0
+# directions list
+dirs = [[0, -1], [1, 0], [0, 1], [-1, 0]]
 
-# Part 2 
-result = 0
-for x in range(1, len(lines) - 1):
-    for y in range(1, len(lines[x]) - 1):
-        if lines[x][y] == "A":
-            print("Looking at: (" + str(x) + "," + str(y) + ")")
-            result += searchXMAS(x, y)
-print(result)
+# get starting position
+guard = []
+# guard direction
+gd = 0
+for y in range(len(lines)):
+    try:
+        x = lines[y].index("^")
+        print "Guard position: (" + str(x) + "," + str(y) + ")"
+        guard = [x, y]
+        break
+    except:
+        pass
+
+def checkpathing(guard, lines):
+    gd = 0
+    path = []
+    while True:
+        # fill current position with X
+        #print guard
+        #print gd
+        lines[guard[1]][guard[0]] = "X" 
+        path.append(guard)
+        #print ".",
+        #printmap(lines)
+
+        # determine target position
+        new = [a + b for a, b in zip(guard, dirs[gd])]
+        #print new
+
+        # check if new is within bounds
+        if new[0] < 0 or new[1] < 0 or new[0] >= len(lines) or new[1] >= len(lines[0]):
+            # ran of the map
+            print "Ran of the map"
+            break
+
+        # check what's there
+        try:
+            # wall, turn right
+            if lines[new[1]][new[0]] == "#":
+                #print "wall"
+                gd = (gd + 1) % 4
+        
+            # empty space, move there
+            elif lines[new[1]][new[0]] in [".", "X"]:
+                temppath = [guard, new]
+                if [guard, new] in [path[i: 2 + i] for i in xrange(len(path))]:
+                    #printmap(lines)
+                    return "looping"
+                guard = list(new)
+
+        except:
+            # ran of the map
+            print "Ran of the map"
+            break
+    return path
+
+loops = 0
+guardstart = list(guard)
+
+# run the original map to find the standard ath
+guardpath = copy.deepcopy(lines)
+path = checkpathing(guardstart, guardpath)
+
+# clear double entries from path
+path = [list(p) for p in set(tuple(element) for element in path)]
+print path
+
+# place an obstacle at every possible location on the path and see if the guard eventually
+# starts looping by checking position plus direction, if they both match an earlier visit
+# we're looping
+#
+# This can be done a lot faster by just looking at the X locations from part 1
+for p in path:
+    # check if the position is not the guards startoing position
+    # initialize guard
+    guard = list(guardstart)
+
+    # copy map
+    testmap = copy.deepcopy(lines)
+    #printmap(testmap)
+
+    # add obstacle
+    testmap[p[1]][p[0]] = "#"
+
+    # run checkpath until it loops or until it exists the mapa
+    if checkpathing(guard, testmap) == "looping":
+        print "Looping"
+        loops += 1
+        print loops
+print loops
